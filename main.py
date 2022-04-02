128021ea70de0f771ae53589484079f47765473a
N1trog3N <antoha323245@gmail.com>
2022-04-02 17:41:51 +0300

    Edited project files (ver. 1.1)

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-import models
 import workshop
-import receipt
 import utils
 
-
-def main():
-    main_workshop = workshop.Workshop(utils.listOfReceipts)
-    result_receipt = workshop.create_receipt()
-    main_workshop.listofreceipts.append(result_receipt)
-    main_workshop.print_receipts()
-
-
-main()
+main_workshop = workshop.Workshop(utils.LIST_OF_RECEIPTS)
+main_workshop.create_receipt()
+result_receipt = main_workshop.print_receipts()
+print(result_receipt[0])
--- workshop.py
+++ workshop.py
@@ -8,43 +8,37 @@ class Workshop:
     def __init__(self, listofreceipts: list):
         self._listofreceipts = listofreceipts
 
-    @property
-    def listofreceipts(self):
-        return self._listofreceipts
-
     def print_receipts(self):
-        all_receipts = self.listofreceipts
-        # return f"Ваша квитанция: {all_receipts}"
-        print(all_receipts)
+        all_receipts = self._listofreceipts
+        return all_receipts
 
+    def create_receipt(self):
+        initials = input("Введите ваше ФИО: ")
+        dateofrepair = random.randint(1, 5)
+        dayofreceive = random.randint(1, 30)
+        dateofreceive = f"{dayofreceive} {utils.LIST_OF_MONTHS[random.randint(0, 9)]}"
+        status = utils.LIST_OF_STATUSES[random.randint(0, 2)]
 
-def create_receipt():
-    initials = input("Введите ваше ФИО: ")
-    dateofrepair = random.randint(1, 5)
-    dayofreceive = random.randint(1, 30)
-    dateofreceive = str(dayofreceive) + utils.spacestr + utils.listOfMonths[random.randint(0, 9)]
-    status = utils.listOfStatuses[random.randint(0, 2)]
+        print("Выберите тип устройства, которое хотите починить: ")
+        for i in range(len(utils.LIST_OF_DEVICES)):
+            print(f"{i + 1}. {utils.LIST_OF_DEVICES[i]}")
+        typeofdevice = utils.LIST_OF_DEVICES[int(input()) - 1]
 
-    print("Выберите тип устройства, которое хотите починить: ")
-    for i in range(len(utils.listOfDevices)):
-        print(f"{i + 1}. {utils.listOfDevices[i]}")
-    typeofdevice = utils.listOfDevices[int(input()) - 1]
+        brand = input("Введите модель: ")
+        description = input("Введите описание поломки: ")
 
-    brand = input("Введите модель: ")
-    description = input("Введите описание поломки: ")
+        if typeofdevice == "Телефон":
+            os = input("Введите OС: ")
+            dev = models.Phone(typeofdevice, brand, os, description)
 
-    if typeofdevice == "Телефон":
-        os = input("Введите OС: ")
-        dev = models.Phone(typeofdevice, brand, os, description)
+        if typeofdevice == "Ноутбук":
+            os = input("Введите OС: ")
+            dateofmanufacturing = input("Введите дату производства: ")
+            dev = models.Notebook(typeofdevice, brand, os, dateofmanufacturing, description)
 
-    if typeofdevice == "Ноутбук":
-        os = input("Введите OС: ")
-        dateofmanufacturing = input("Введите дату производства: ")
-        dev = models.Notebook(typeofdevice, brand, os, dateofmanufacturing, description)
+        if typeofdevice == "Телевизор":
+            diagonal = int(input("Введите диагональ экрана: "))
+            dev = models.TV(typeofdevice, brand, diagonal, description)
 
-    if typeofdevice == "Телевизор":
-        diagonal = int(input("Введите диагональ экрана: "))
-        dev = models.TV(typeofdevice, brand, diagonal, description)
-
-    new_receipt = receipt.Receipt(typeofdevice, dateofreceive, dateofrepair, initials, status)
-    return new_receipt.__str__()
+        new_receipt = receipt.Receipt(typeofdevice, dateofreceive, dateofrepair, initials, status)
+        self._listofreceipts.append(new_receipt)
